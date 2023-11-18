50eb92716e4ce0cc3236ff696a88c92ac7b19b26
Avelinka <131503535+Avelinka@users.noreply.github.com>
2023-11-18 02:49:10 +0200

    Initial commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,10 @@
+import { Main } from './Helper/Layout';
+import { GlobalStyle } from './GlobalStyle';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Main>
+      <GlobalStyle />
+    </Main>
   );
 };
